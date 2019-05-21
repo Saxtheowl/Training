69c3cfea7f2553e464eb67398ebc47fd91e69041
saxtheowl <saxtheowl@gmail.com>
2019-05-21 21:00:56 +0200

    a

---
+++ my_blockchain/blockchain.py
@@ -0,0 +1,27 @@
+
+import hashlib
+import json
+from time import time
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []fff
+        self.current_transactions = []
+        
+    def new_block(self):
+        # Creates a new Block and adds it to the chain
+        pass
+    
+    def new_transaction(self):
+        # Adds a new transaction to the list of transactions
+        pass
+    
+    @staticmethod
+    def hash(block):
+        # Hashes a Block
+        pass
+
+    @property
+    def last_block(self):
+        # Returns the last Block in the chain
+        pass
