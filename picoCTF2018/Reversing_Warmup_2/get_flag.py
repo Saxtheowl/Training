a7981221afefb28a240ef471c8d8c17f142600cf
saxtheowl <saxtheowl@gmail.com>
2019-06-27 19:33:44 +0200

    ok

---
+++ picoCTF2018/Reversing_Warmup_2/get_flag.py
@@ -0,0 +1,4 @@
+import base64
+coded_string = 'dGg0dF93NHNfczFtcEwz'
+print base64.b64decode(coded_string)
+print coded_string
--- react-bootcamp/intro/src/components/App.js
+++ react-bootcamp/intro/src/components/App.js
@@ -19,7 +19,7 @@ class App extends Component {
             <li><Link to='/about'>About</Link></li>
             <li><Link to='/topics'>Topics</Link></li>
           </ul>
-          <Route path='/' component={Home} />
+          <Route exact path='/' component={Home} />
           <Route path='/about' component={About} />
           <Route path='/topics' component={Topics} />
         </div>
