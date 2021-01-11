84fdc28c0c2b97c1fcbd5bd5beaf9253b68f3c95
Saxtheowl <Saxtheowl@gmail.com>
2021-01-11 15:25:10 +0100

    ok

---
+++ programmingbitcoin/code-ch05/dat_run.py
@@ -0,0 +1,10 @@
+############## PLEASE RUN THIS CELL FIRST! ###################
+
+# import everything and define a test runner function
+from importlib import reload
+from helper import run
+from tx import dat_test
+import ecc
+import helper
+import script
+import tx
--- programmingbitcoin/code-ch05/tx.py
+++ programmingbitcoin/code-ch05/tx.py
@@ -17,18 +17,17 @@ from script import Script
 # tag::source7[]
 class TxFetcher:
     cache = {}
-
     @classmethod
     def get_url(cls, testnet=False):
         if testnet:
-            return 'http://testnet.programmingbitcoin.com'
+            return 'https://blockstream.info/testnet/api/'
         else:
-            return 'http://mainnet.programmingbitcoin.com'
+            return 'https://blockstream.info/api/'
 
     @classmethod
     def fetch(cls, tx_id, testnet=False, fresh=False):
         if fresh or (tx_id not in cls.cache):
-            url = '{}/tx/{}.hex'.format(cls.get_url(testnet), tx_id)
+            url = '{}/tx/{}/hex'.format(cls.get_url(testnet), tx_id)
             response = requests.get(url)
             try:
                 raw = bytes.fromhex(response.text.strip())
@@ -116,25 +115,19 @@ class Tx:
         # parse num_outputs number of TxOuts
         # locktime is an integer in 4 bytes, little-endian
         # return an instance of the class (see __init__ for args)
-        """
-        version = little_endian_to_int(s.read(4))
-        print('dat version:{}'.format(version))
-        num_inputs = read_varint(s)
-        print('dat version:{}'.format(num_inputs))
-        tx_ins = 0
-        tx_outs = 0
-        locktime = 0
-        return cls(version, tx_ins, tx_outs, locktime)
-#        raise NotImplementedError
-"""
-
-    def parse(cls, s, testnet=False):
         version = little_endian_to_int(s.read(4))
         num_inputs = read_varint(s)
         inputs = []
         for _ in range(num_inputs):
             inputs.append(TxIn.parse(s))
-            return cls(version, inputs, None, None, testnet='testnet')
+        num_outputs = read_varint(s)
+        outputs = []
+        while (num_outputs > 0):
+            outputs.append(TxOut.parse(s))
+            num_outputs -= 1
+        locktime = little_endian_to_int(s.read(4))
+        return cls(version, inputs, outputs, locktime, testnet=testnet)
+
     # tag::source6[]
     def serialize(self):
         '''Returns the byte serialization of the transaction'''
@@ -178,21 +171,21 @@ class TxIn:
 
     @classmethod
     def parse(cls, s):
-        '''Takes a byte stream and parses the tx_input at the start
-        return a TxIn object
-        '''
         prev_tx = s.read(32)[::-1]
         prev_index = little_endian_to_int(s.read(4))
         script_sig = Script.parse(s)
         sequence = little_endian_to_int(s.read(4))
         return cls(prev_tx, prev_index, script_sig, sequence)
+
+        '''Takes a byte stream and parses the tx_input at the start
+        return a TxIn object
+        '''
         # prev_tx is 32 bytes, little endian
         # prev_index is an integer in 4 bytes, little endian
         # use Script.parse to get the ScriptSig
         # sequence is an integer in 4 bytes, little-endian
         # return an instance of the class (see __init__ for args)
 #        raise NotImplementedError
-
     # tag::source5[]
     def serialize(self):
         '''Returns the byte serialization of the transaction input'''
@@ -212,6 +205,7 @@ class TxIn:
         Returns the amount in satoshi.
         '''
         tx = self.fetch_tx(testnet=testnet)
+        print('lul2')
         return tx.tx_outs[self.prev_index].amount
 
     def script_pubkey(self, testnet=False):
@@ -239,15 +233,18 @@ class TxOut:
         '''Takes a byte stream and parses the tx_output at the start
         return a TxOut object
         '''
+        amount = little_endian_to_int(s.read(8))
+        script_pubkey = Script.parse(s)
         # amount is an integer in 8 bytes, little endian
         # use Script.parse to get the ScriptPubKey
         # return an instance of the class (see __init__ for args)
-        raise NotImplementedError
+        return cls(amount, script_pubkey)
 
     # tag::source4[]
     def serialize(self):  # <1>
         '''Returns the byte serialization of the transaction output'''
         result = int_to_little_endian(self.amount, 8)
+        print('lul')
         result += self.script_pubkey.serialize()
         return result
     # end::source4[]
@@ -255,7 +252,6 @@ class TxOut:
 
 class TxTest(TestCase):
     cache_file = '../tx.cache'
-
     @classmethod
     def setUpClass(cls):
         # fill with cache so we don't have to be online to run these tests
@@ -266,18 +262,6 @@ class TxTest(TestCase):
         stream = BytesIO(raw_tx)
         tx = Tx.parse(stream)
         self.assertEqual(tx.version, 1)
-
-    def test_parse_inputs(self):
-        raw_tx = bytes.fromhex('0100000002813f79011acb80925dfe69b3def355fe914bd1d96a3f5f71bf8303c6a989c7d1000000006b483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278afeffffff02a135ef01000000001976a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac99c39800000000001976a9141c4bc762dd5423e332166702cb75f40df79fea1288ac19430600')
-        stream = BytesIO(raw_tx)
-        tx = Tx.parse(stream)
-        self.assertEqual(len(tx.tx_ins), 1)
-        want = bytes.fromhex('d1c789a9c60383bf715f3f6ad9d14b91fe55f3deb369fe5d9280cb1a01793f81')
-        self.assertEqual(tx.tx_ins[0].prev_tx, want)
-        self.assertEqual(tx.tx_ins[0].prev_index, 0)
-        want = bytes.fromhex('6b483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278a')
-        self.assertEqual(tx.tx_ins[0].script_sig.serialize(), want)
-        self.assertEqual(tx.tx_ins[0].sequence, 0xfffffffe)
 
     def test_parse_outputs(self):
         raw_tx = bytes.fromhex('0100000001813f79011acb80925dfe69b3def355fe914bd1d96a3f5f71bf8303c6a989c7d1000000006b483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278afeffffff02a135ef01000000001976a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac99c39800000000001976a9141c4bc762dd5423e332166702cb75f40df79fea1288ac19430600')
@@ -308,3 +292,43 @@ class TxTest(TestCase):
         stream = BytesIO(raw_tx)
         tx = Tx.parse(stream)
         self.assertEqual(tx.fee(), 140500)
+"""
+def dat_test(self):
+    dat_test_raw_txt = bytes.fromhex('010000000456919960ac691763688d3d3bcea9ad6ecaf875df5339e148a1fc61c6ed7a069e010000006a47304402204585bcdef85e6b1c6af5c2669d4830ff86e42dd205c0e089bc2a821657e951c002201024a10366077f87d6bce1f7100ad8cfa8a064b39d4e8fe4ea13a7b71aa8180f012102f0da57e85eec2934a82a585ea337ce2f4998b50ae699dd79f5880e253dafafb7feffffffeb8f51f4038dc17e6313cf831d4f02281c2a468bde0fafd37f1bf882729e7fd3000000006a47304402207899531a52d59a6de200179928ca900254a36b8dff8bb75f5f5d71b1cdc26125022008b422690b8461cb52c3cc30330b23d574351872b7c361e9aae3649071c1a7160121035d5c93d9ac96881f19ba1f686f15f009ded7c62efe85a872e6a19b43c15a2937feffffff567bf40595119d1bb8a3037c356efd56170b64cbcc160fb028fa10704b45d775000000006a47304402204c7c7818424c7f7911da6cddc59655a70af1cb5eaf17c69dadbfc74ffa0b662f02207599e08bc8023693ad4e9527dc42c34210f7a7d1d1ddfc8492b654a11e7620a0012102158b46fbdff65d0172b7989aec8850aa0dae49abfb84c81ae6e5b251a58ace5cfeffffffd63a5e6c16e620f86f375925b21cabaf736c779f88fd04dcad51d26690f7f345010000006a47304402200633ea0d3314bea0d95b3cd8dadb2ef79ea8331ffe1e61f762c0f6daea0fabde022029f23b3e9c30f080446150b23852028751635dcee2be669c2a1686a4b5edf304012103ffd6f4a67e94aba353a00882e563ff2722eb4cff0ad6006e86ee20dfe7520d55feffffff0251430f00000000001976a914ab0c0b2e98b1ab6dbf67d4750b0a56244948a87988ac005a6202000000001976a9143c82d7df364eb6c75be8c80df2b3eda8db57397088ac46430600')
+    stream = BytesIO(dat_test_raw_txt)
+    tx = Tx.parse(stream)
+"""
+
+"""
+ver 01000000
+nb_input 04
+
+prev_tx1 56919960ac691763688d3d3bcea9ad6ecaf875df5339e148a1fc61c6ed7a069e
+prev_idx1 01000000
+scriptsig1 6a47304402204585bcdef85e6b1c6af5c2669d4830ff86e42dd205c0e089bc2a821657e951c002201024a10366077f87d6bce1f7100ad8cfa8a064b39d4e8fe4ea13a7b71aa8180f012102f0da57e85eec2934a82a585ea337ce2f4998b50ae699dd79f5880e253dafafb7
+sequence1 feffffff
+
+prev_tx2 eb8f51f4038dc17e6313cf831d4f02281c2a468bde0fafd37f1bf882729e7fd3
+prev_idx2 00000000
+scriptsig2 6a47304402207899531a52d59a6de200179928ca900254a36b8dff8bb75f5f5d71b1cdc26125022008b422690b8461cb52c3cc30330b23d574351872b7c361e9aae3649071c1a7160121035d5c93d9ac96881f19ba1f686f15f009ded7c62efe85a872e6a19b43c15a2937
+sequence2 feffffff
+
+567bf40595119d1bb8a3037c356efd56170b64cbcc160fb028fa10704b45d775
+00000000
+6a47304402204c7c7818424c7f7911da6cddc59655a70af1cb5eaf17c69dadbfc74ffa0b662f02207599e08bc8023693ad4e9527dc42c34210f7a7d1d1ddfc8492b654a11e7620a0012102158b46fbdff65d0172b7989aec8850aa0dae49abfb84c81ae6e5b251a58ace5c
+
+sequence 3 feffffff
+
+d63a5e6c16e620f86f375925b21cabaf736c779f88fd04dcad51d26690f7f345
+01000000
+6a47304402200633ea0d3314bea0d95b3cd8dadb2ef79ea8331ffe1e61f762c0f6daea0fabde022029f23b3e9c30f080446150b23852028751635dcee2be669c2a1686a4b5edf304012103ffd6f4a67e94aba353a00882e563ff2722eb4cff0ad6006e86ee20dfe7520d55
+sequence 4 feffffff
+
+nb_input 02
+amount1 51430f0000000000
+scriptpubkey1 1976a914ab0c0b2e98b1ab6dbf67d4750b0a56244948a87988ac
+amount2 005a620200000000
+scriptpubkey2 1976a9143c82d7df364eb6c75be8c80df2b3eda8db57397088ac
+locktime 46430600
+
+"""
