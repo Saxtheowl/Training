29fa9879929d56787f9cc06a5aa3d264fa870df4
Saxtheowl <Saxtheowl@gmail.com>
2021-03-02 01:46:01 +0100

    ok

--- programmingbitcoin/code-ch13/dat_transact15.py
+++ programmingbitcoin/code-ch13/dat_transact15.py
@@ -5,26 +5,15 @@ from ecc import PrivateKey
 
 # make a tx from a p2sh address
 
-secret = little_endian_to_int(hash256(b'dat_test_private_key'))
-
-private_key = PrivateKey(secret)
-
-public_key = print(private_key.point.address(testnet=False)) #1FiiobU54Sja2XB1pqtNzKB2VAkAzm2Ssi 
-public_key = print(private_key.point.address(testnet=True)) # mvEg6eZ3sUApodedYQrkpEPMMALsr1K1k1
-
 secret1 = little_endian_to_int(hash256(b'dat_test_private_key_p2sh1'))
 
 secret2 = little_endian_to_int(hash256(b'dat_test_private_key2_p2sh2'))
 
-## first we create the scriptPubKey of the redeemscript or redeem script
-
-# we decode our previous pubkey to put them in script commands
-
 private_key1 = PrivateKey(secret1)
 private_key2 = PrivateKey(secret2)
 
-public_key1 = decode_base58('mvEg6eZ3sUApodedYQrkpEPMMALsr1K1k1')
-public_key2 = decode_base58('mwjg9Y1jeFdNu7DQBAW7DUbJqj6jMmhd74')
+public_key1 = decode_base58('mfchv5Xq63UuQTAVwzqhWVHAZVkiZrhDKh')
+public_key2 = decode_base58('ms4kdy8Hn4WfxK2J2VpbonTit5NeNy6vJS')
 
 #redeem script as op code
 
@@ -33,9 +22,8 @@ dat_redeem_script_op = Script([0x52, public_key1, public_key2, 0x52, 0xae])
 # we serialize it in binary
 
 dat_redeem_script_serialized = dat_redeem_script_op.serialize()
-print('dat_redeem_script_serialized:{}'.format(dat_redeem_script_serialized))
 
-prev_tx = bytes.fromhex('74aa4bb3ab5bd9966b8a06c442c37eb9177339dfdb1b1594a8d174e1dc201ff5')
+prev_tx = bytes.fromhex('f387eba7393e07b4c2134cd29551b7294a992c9bae129b347b64ba6650522acb')
 prev_index = 0
 
 target_h160 = decode_base58('mvEg6eZ3sUApodedYQrkpEPMMALsr1K1k1')
@@ -46,25 +34,17 @@ target_output = TxOut(amount=target_amount, script_pubkey=target_script)
 
 tx_in = TxIn(prev_tx, prev_index)
 
-# output address that will receive our sat
-
 tx_obj = Tx(1, [tx_in], [target_output], 0, True)
 
-# now we sign the p2sh tx
-
 z = tx_obj.sig_hash(0)
-#private_key = PrivateKey(little_endian_to_int(hash256(b'dat_test_private_key')))
 private_key1 = PrivateKey(secret1)
 private_key2 = PrivateKey(secret2)
 der1 = private_key1.sign(z).der()
 der2 = private_key2.sign(z).der()
 sig1 = der1 + SIGHASH_ALL.to_bytes(1, 'big')
 sig2 = der2 + SIGHASH_ALL.to_bytes(1, 'big')
-#sec = private_key.point.sec()
-#print(sec.hex())
 script_sig = Script([0x00, sig1, sig2, dat_redeem_script_serialized])
 print('script_sig=:{}'.format(script_sig))
-
 tx_obj.tx_ins[0].script_sig = script_sig
 print(tx_obj.serialize().hex())
 
---
+++ programmingbitcoin/code-ch13/dat_transact8.py
@@ -0,0 +1,84 @@
+from helper import decode_base58, little_endian_to_int, hash256, SIGHASH_ALL, hash160, h160_to_p2sh_address
+from script import p2pkh_script, p2sh_script, Script
+from tx import TxIn, TxOut, Tx
+from ecc import PrivateKey
+
+# we send to a p2sh address
+
+secret1 = little_endian_to_int(hash256(b'dat_test_private_key_p2sh1'))
+
+secret2 = little_endian_to_int(hash256(b'dat_test_private_key2_p2sh2'))
+
+private_key1 = PrivateKey(secret1)
+private_key2 = PrivateKey(secret2)
+
+print(private_key1.point.address(testnet=True)) #mfchv5Xq63UuQTAVwzqhWVHAZVkiZrhDKh 
+print(private_key2.point.address(testnet=True)) #ms4kdy8Hn4WfxK2J2VpbonTit5NeNy6vJS
+
+## first we create the scriptPubKey of the redeemscript or redeem script
+
+# we decode our previous pubkey to put them in script commands
+
+public_key1 = decode_base58('mfchv5Xq63UuQTAVwzqhWVHAZVkiZrhDKh')
+public_key2 = decode_base58('ms4kdy8Hn4WfxK2J2VpbonTit5NeNy6vJS')
+
+#redeem script as op code
+
+dat_redeem_script_op = Script([0x52, public_key1, public_key2, 0x52, 0xae])
+
+# we serialize it in binary
+
+dat_redeem_script_serialized = dat_redeem_script_op.serialize()
+print('dat_redeem_script_serialized:{}'.format(dat_redeem_script_serialized))
+
+# hash 160
+
+dat_redeem_script_h160 = hash160(dat_redeem_script_serialized)
+
+# we get the address
+
+dat_redeem_script_address = h160_to_p2sh_address(dat_redeem_script_h160, testnet=True)
+
+# get it back in bytes
+
+dat_target_h160 = decode_base58(dat_redeem_script_address)
+
+# use the result to build our p2sh_script
+
+target_script = p2sh_script(dat_target_h160)
+
+print(target_script)
+print('is p2sh: {}'.format(target_script.is_p2sh_script_pubkey()))
+
+print(dat_redeem_script_h160)
+print()
+print('redeem script address: {}'.format(dat_redeem_script_address))
+
+# UTXO that we gonna receive
+
+prev_tx = bytes.fromhex('637e37f1fb26480b687c522ed44ab84fd236d4e4867c3338f29f244d93246768')
+prev_index = 0
+
+# create the txin with the output of the UTXO we have been given
+
+tx_in = TxIn(prev_tx, prev_index)
+
+# output address that will receive our sat
+
+target_amount = int(0.00009 * 100000000)
+target_output = TxOut(amount=target_amount, script_pubkey=target_script)
+
+tx_obj = Tx(1, [tx_in], [target_output], 0, True)
+
+# now we sign the transaction
+
+z = tx_obj.sig_hash(0)
+private_key = PrivateKey(little_endian_to_int(hash256(b'dat_test_private_key')))
+der = private_key.sign(z).der()
+sig = der + SIGHASH_ALL.to_bytes(1, 'big')
+sec = private_key.point.sec()
+script_sig = Script([sig, sec])
+tx_obj.tx_ins[0].script_sig = script_sig
+print(tx_obj.serialize().hex())
+
+# end
